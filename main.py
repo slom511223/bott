4b19fd48f421e10f8ee82684ccd688d91be25de1
slom511223 <slom511332@gmail.com>
2026-04-18 16:34:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -137,8 +137,7 @@ async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
-app.add_handler(CallbackQueryHandler(button))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle))
+app.add_handler(CallbackQueryHandler(b
 
 print("Bot is running...")
 app.run_polling()
