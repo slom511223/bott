4a9f46a48d166cd5d5729f5d07ba2ecd0e5b0959
slom511223 <slom511332@gmail.com>
2026-04-18 16:56:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# 🔑 حط التوكن حقك هنا
 TOKEN = "8623464854:AAFtyVm5HJu-K_qJyR5qVRefwdTG-4GHnBY"
 
-# أمر /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("البوت شغال 100% 🔥")
 
-# تشغيل البوت
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
 
-print("Bot started...")
-app.run_polling()
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot started...")
+    app.run_polling()
+
+if name == "__main__":
+    main()
