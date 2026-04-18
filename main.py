9e4f94c6f96d2ee39dc404d3c12f32d363776265
slom511223 <slom511332@gmail.com>
2026-04-18 17:31:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-TOKEN = os.getenv("8623464854:AAFtyVm5HJu-K_qJyR5qVRefwdTG-4GHnBY")
+TOKEN = os.getenv"8623464854:AAFtyVm5HJu-K_qJyR5qVRefwdTG-4GHnBY"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("البوت شغال على Railway 🔥")
